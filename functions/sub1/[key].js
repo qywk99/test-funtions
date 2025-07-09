d7948191ae8e5343c66fca6d8d26a0429f0e8fca
qywk99 <2529107424@qq.com>
2025-07-09 18:27:11 +0800

    update

--- functions/sub1/[key].js
+++ functions/sub1/[key].js
@@ -1,7 +1,7 @@
 export async function onRequest(context) {
     return new Response(JSON.stringify({
         Code: 200,
-        Message: "sub2/key"
+        Message: "sub1/key"
     }), {
         status: 200,
     });
